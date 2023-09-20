624c2ad6719e84a4e3149ae45b112a26dba78d37
Matt <mjruddy94@gmail.com>
2023-09-20 10:40:34 -0400

    try something diff

--- services/sendEmail.js
+++ services/sendEmail.js
@@ -1,14 +1,17 @@
 var nodemailer = require("nodemailer");
 //-----------------------------------------------------------------------------
 export async function sendMail(subject, toEmail, otpText) {
+  const email = process.env.NODEMAILER_EMAIL;
+  const pw = process.env.NODEMAILER_PW;
+  console.log({ email, pw });
   var transporter = nodemailer.createTransport("SMTP", {
-    service: "gmail",
+    host: "smtp.gmail.com",
     port: 465,
-    secure: true,
     auth: {
       user: process.env.NODEMAILER_EMAIL,
       pass: process.env.NODEMAILER_PW,
     },
+    secure: true,
   });
 
   var mailOptions = {
