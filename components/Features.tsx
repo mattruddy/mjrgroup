c439b7ff80d5ba3192e39f466e6882798473b14f
Matt <mjruddy94@gmail.com>
2023-10-14 18:12:38 -0400

    change background

--- components/Features.tsx
+++ components/Features.tsx
@@ -9,6 +9,7 @@ import {
   StackDivider,
   Icon,
   useColorModeValue,
+  Box,
 } from "@chakra-ui/react";
 import { IoManSharp } from "react-icons/io5";
 import { BiSolidBusiness } from "react-icons/bi";
@@ -46,74 +47,74 @@ const Feature = ({ text, icon, iconBg, href }: FeatureProps) => {
 export default function SplitWithImage() {
   const { title, subTitle, imageSrc } = PAGE_DATA.page.home.sectionTwo;
   return (
-    <Container
-      maxW={"5xl"}
-      py={12}
-      bg={useColorModeValue("gray.100", "gray.700")}
-    >
-      <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
-        <Stack spacing={4}>
-          <Text
-            textTransform={"uppercase"}
-            color={"blue.400"}
-            fontWeight={600}
-            fontSize={"sm"}
-            bg={useColorModeValue("blue.50", "blue.900")}
-            p={2}
-            alignSelf={"flex-start"}
-            rounded={"md"}
-          >
-            Our Services
-          </Text>
-          <Heading>{title}</Heading>
-          <Text color={"gray.500"} fontSize={"lg"}>
-            {subTitle}
-          </Text>
-          <Stack
-            spacing={4}
-            divider={
-              <StackDivider
-                borderColor={useColorModeValue("gray.100", "gray.700")}
-              />
-            }
-          >
-            <Feature
-              icon={<Icon as={TbPigMoney} color={"yellow.500"} w={5} h={5} />}
-              iconBg={useColorModeValue("yellow.100", "yellow.900")}
-              text={"Tax Preparation"}
-              href="tax"
-            />
-            <Feature
-              icon={<Icon as={IoManSharp} color={"orange.500"} w={5} h={5} />}
-              iconBg={useColorModeValue("orange.100", "orange.900")}
-              text={"IRS Representation"}
-              href="representation"
-            />
-            <Feature
-              icon={
-                <Icon as={BiSolidBusiness} color={"purple.500"} w={5} h={5} />
+    <Box w="100%" bg={useColorModeValue("gray.100", "gray.700")}>
+      <Container maxW={"5xl"} py={12}>
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+          <Stack spacing={4}>
+            <Text
+              textTransform={"uppercase"}
+              color={"blue.400"}
+              fontWeight={600}
+              fontSize={"sm"}
+              bg={useColorModeValue("blue.50", "blue.900")}
+              p={2}
+              alignSelf={"flex-start"}
+              rounded={"md"}
+            >
+              Our Services
+            </Text>
+            <Heading>{title}</Heading>
+            <Text color={"gray.500"} fontSize={"lg"}>
+              {subTitle}
+            </Text>
+            <Stack
+              spacing={4}
+              divider={
+                <StackDivider
+                  borderColor={useColorModeValue("gray.100", "gray.700")}
+                />
               }
-              iconBg={useColorModeValue("purple.100", "purple.900")}
-              text={"Business Consultation"}
-              href="business"
-            />
-            <Feature
-              icon={<Icon as={SiQuickbooks} color={"green.500"} w={5} h={5} />}
-              iconBg={useColorModeValue("green.100", "green.900")}
-              text={"QuickBooks Training"}
-              href="quickbooks"
-            />
+            >
+              <Feature
+                icon={<Icon as={TbPigMoney} color={"yellow.500"} w={5} h={5} />}
+                iconBg={useColorModeValue("yellow.100", "yellow.900")}
+                text={"Tax Preparation"}
+                href="tax"
+              />
+              <Feature
+                icon={<Icon as={IoManSharp} color={"orange.500"} w={5} h={5} />}
+                iconBg={useColorModeValue("orange.100", "orange.900")}
+                text={"IRS Representation"}
+                href="representation"
+              />
+              <Feature
+                icon={
+                  <Icon as={BiSolidBusiness} color={"purple.500"} w={5} h={5} />
+                }
+                iconBg={useColorModeValue("purple.100", "purple.900")}
+                text={"Business Consultation"}
+                href="business"
+              />
+              <Feature
+                icon={
+                  <Icon as={SiQuickbooks} color={"green.500"} w={5} h={5} />
+                }
+                iconBg={useColorModeValue("green.100", "green.900")}
+                text={"QuickBooks Training"}
+                href="quickbooks"
+              />
+            </Stack>
           </Stack>
-        </Stack>
-        <Flex>
-          <Image
-            rounded={"md"}
-            alt={"feature image"}
-            src={imageSrc}
-            objectFit={"cover"}
-          />
-        </Flex>
-      </SimpleGrid>
-    </Container>
+          <Flex>
+            <Image
+              rounded={"md"}
+              alt={"feature image"}
+              src={imageSrc}
+              objectFit={"cover"}
+            />
+          </Flex>
+        </SimpleGrid>
+      </Container>
+    </Box>
   );
 }
