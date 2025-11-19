4f3ed39a9caed51fac6bf7ddba4a1c35245b6c25
mattruddy <mjruddy94@gmail.com>
2025-11-19 15:24:30 -0500

    dfsgfd

--- components/Quote.tsx
+++ components/Quote.tsx
@@ -1,5 +1,4 @@
-import { Box, HStack, Text, VStack } from "@chakra-ui/layout";
-import { Icon } from "@chakra-ui/react";
+import { Box, HStack, Icon, VStack, Text } from "@chakra-ui/react";
 import React from "react";
 import { FaQuoteLeft } from "react-icons/fa";
 
